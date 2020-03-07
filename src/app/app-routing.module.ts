ac96b8c4c85fa48b3260d22da26873f5154a975d
qcowco <kamil.szuberski.g@gmail.com>
2020-03-07 19:04:04 +0100

    Set routing for viewing and writing notes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {WriteNoteComponent} from './write-note/write-note.component';
+import {ViewNoteComponent} from './view-note/view-note.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: WriteNoteComponent},
+  {path: ':id', component: ViewNoteComponent},
+  {path: ':id/:key', component: ViewNoteComponent},
+  {path: '*', component: WriteNoteComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
