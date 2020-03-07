dc3536c1d95c0c6858e45e6956faf1f2bba96558
qcowco <kamil.szuberski.g@gmail.com>
2020-03-07 19:05:43 +0100

    Minor file updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,28 @@ import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { WriteNoteComponent } from './write-note/write-note.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ViewNoteComponent } from './view-note/view-note.component';
+import {MatButtonModule, MatDialogModule} from '@angular/material';
+import {FormsModule} from '@angular/forms';
+import { OptionsDialogComponent } from './options-dialog/options-dialog.component';
+import { ResultDialogComponent } from './result-dialog/result-dialog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WriteNoteComponent
+    WriteNoteComponent,
+    ViewNoteComponent,
+    OptionsDialogComponent,
+    ResultDialogComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    FormsModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
