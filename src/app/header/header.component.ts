2d834aef2bd2fb49078159cc5e4bf9e1947e66ac
qcowco <kamil.szuberski.g@gmail.com>
2020-03-20 22:15:08 +0100

    Add folder/user dialog options to header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {SharedNoteService} from '../service/shared-note/shared-note.service';
 import {Note} from '../model/note/note';
+import {SecurityService} from '../service/security-service/security.service';
+import {MatDialog} from '@angular/material';
+import {UserDialogComponent} from '../user-dialog/user-dialog.component';
+import {FolderDialogComponent} from '../folder-dialog/folder-dialog.component';
 
 @Component({
   selector: 'app-header',
@@ -9,18 +13,32 @@ import {Note} from '../model/note/note';
 })
 export class HeaderComponent implements OnInit {
   private emittedNote: Note;
+  private username: string;
 
-  constructor(private sharedNote: SharedNoteService) {
+  constructor(private sharedNote: SharedNoteService, private securityService: SecurityService, private dialog: MatDialog) {
     sharedNote.changeEmmited.subscribe(note => this.emittedNote = note);
   }
 
   ngOnInit() {
+    if (this.securityService.isLogged()) {
+      this.username = this.securityService.getLoggedUser();
+    }
   }
+
 
   getName() {
     if (this.emittedNote != null) {
       return this.emittedNote.name;
     }
+  }
+
+  openUserDialog() {
+    return this.dialog.open(UserDialogComponent, { width: '350px', height: '280px', panelClass: 'user-dialog' }).afterClosed().toPromise();
+  }
+
+  openFolderDialog() {
+    return this.dialog.open(FolderDialogComponent, { width: '800px', height: '600px', data:  this.emittedNote})
+      .afterClosed().toPromise();
   }
 
 }
