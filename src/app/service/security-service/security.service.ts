c9133532d1c97109e1d430ad3faba3786aa4b5b6
qcowco <kamil.szuberski.g@gmail.com>
2020-03-20 21:40:01 +0100

    Add REST authentication layer

---
+++ src/app/service/security-service/security.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SecurityService {
+  private readonly userUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.userUrl = 'http://localhost:8080';
+  }
+
+  isLogged(): boolean {
+    return localStorage.getItem('access_token') != null;
+  }
+
+  public login(username: string, password: string) {
+    return this.http.post<JwtUser>(`${this.userUrl}/login`, { username, password });
+  }
+
+  public saveToken(token: string) {
+    localStorage.setItem('access_token', token);
+  }
+
+  public logout() {
+    localStorage.removeItem('access_token');
+  }
+
+  public registerUser(username: string, password: string) {
+    return this.http.post(`${this.userUrl}/register`, { username, password });
+  }
+
+  public getLoggedUser(): string {
+    const token = localStorage.getItem('access_token');
+    if (token) {
+      let payload = token.split('.')[1];
+      payload = window.atob(payload);
+      return JSON.parse(payload).sub;
+    }
+  }
+}
+
+export interface JwtUser {
+  token: string;
+}
