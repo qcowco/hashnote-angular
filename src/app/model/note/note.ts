9f052dfe287e9072fb91cb85d4a6a345d449a86a
qcowco <kamil.szuberski.g@gmail.com>
2020-03-20 21:35:09 +0100

    Make note message optional

--- src/app/model/note/note.ts
+++ src/app/model/note/note.ts
@@ -1,7 +1,7 @@
 export class Note {
   public id: string;
   public name: string;
-  public message: string;
+  public message?: string;
 
   constructor(name: string, message: string) {
     this.name = name;
