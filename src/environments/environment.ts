f0d2cb75b56d3e12831d771cd694ecd28c716b57
qcowco <kamil.szuberski.g@gmail.com>
2020-04-12 18:37:35 +0200

    Add api/url environmental variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,14 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  api: 'http://localhost:8080/api/v1',
+  url: 'http://localhost:8080'
 };
 
 /*
  * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ * to ignore zone related loginError stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
  *
  * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
+ * on performance if an loginError is thrown.
  */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+// import 'zone.js/dist/zone-loginError';  // Included with Angular CLI.
