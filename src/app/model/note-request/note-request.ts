079c3a2b4307d56a42c1ac6203dd9410444c149e
qcowco <kamil.szuberski.g@gmail.com>
2020-04-15 00:10:48 +0200

    Remove redundant maxVisits variable

--- src/app/model/note-request/note-request.ts
+++ src/app/model/note-request/note-request.ts
@@ -4,12 +4,10 @@ export class NoteRequest {
   public noteDto: Note;
   public method: string;
   public minutesToExpiration?: number;
-  public maxVisits?: number;
 
-  constructor(noteDto: Note, method: string, minutesToExpiration: number, maxVisits: number) {
+  constructor(noteDto: Note, method: string, minutesToExpiration: number) {
     this.noteDto = noteDto;
     this.method = method;
     this.minutesToExpiration = minutesToExpiration;
-    this.maxVisits = maxVisits;
   }
 }
